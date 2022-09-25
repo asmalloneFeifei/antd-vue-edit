29d9326fa07dd8ee50ea45aaa5ea0c2a38c6cab2
leeyer <1027848185@qq.com>
2022-09-25 16:32:21 +0800

    add

--- src/components/DemoList.vue
+++ src/components/DemoList.vue
@@ -1,24 +1,13 @@
 <template>
   <div>
     <a-table :columns="columns" :data-source="data" bordered>
-      <template
-        v-for="col in ['name', 'age', 'address']"
-        :slot="col"
-        slot-scope="text, record, index"
-      >
-        <div :key="col">
-          <a-input
-            v-if="record.editable"
-            style="margin: -5px 0"
-            :value="text"
-            @change="e => handleChange(e.target.value, record.key, col)"
-          />
-          <template v-else>{{ text }}</template>
-        </div>
+      <a slot="name" slot-scope="text">{{ text }}</a>
+      <template v-for="col in columns" :slot="col" slot-scope="text, record, index">
+        <span>{{ text }}</span>
       </template>
 
       <template slot="operation" slot-scope="text, record, index">
-        <a @click="getItem(record, false)">编辑</a>
+        <a @click="getItem(record, index)">编辑</a>
       </template>
     </a-table>
     <a-modal
@@ -35,57 +24,64 @@
         <a-button key="submit" type="primary" :loading="confirmLoading" @click="handleOk">确认</a-button>
       </template>
       <a-form :form="form" :label-col="{ span: 5 }" :wrapper-col="{ span: 12 }">
-        <a-form-item label="字段名称">
+        <a-form-item label="字段名称" :labelCol="labelCol" :wrapperCol="wrapperCol">
           <a-input
             v-decorator="['name', { rules: [{ required: true, message: '请填写字段名称' }] }]"
             placeholder="请填写字段"
           />
         </a-form-item>
-        <a-form-item label="字段类型">
-          <a-select
-            default-value="1"
-            v-decorator="['type', { rules: [{ required: true, message: '请选择字段类型' }] }]"
-          >
-            <a-select-option value="1">单行文本</a-select-option>
-            <a-select-option value="2">日期</a-select-option>
-            <a-select-option value="3">单选下拉</a-select-option>
+        <a-form-item label="字段类型" :labelCol="labelCol" :wrapperCol="wrapperCol">
+          <a-select v-decorator="['type', { rules: [{ required: true, message: '请选择字段类型' }] }]">
+            <a-select-option value="单行文本">单行文本</a-select-option>
+            <a-select-option value="日期">日期</a-select-option>
+            <a-select-option value="单选下拉">单选下拉</a-select-option>
           </a-select>
         </a-form-item>
-        <a-form-item label="字段名称">
+        <a-form-item
+          label="字段名称"
+          v-if="form.getFieldsValue().type =='单行文本'"
+          :labelCol="labelCol"
+          :wrapperCol="wrapperCol"
+        >
           <a-input
             v-decorator="['text', { rules: [{ required: true, message: '请填写文本' }] }]"
             placeholder="请填写文本"
           />
         </a-form-item>
-        <a-form-item label="日期类型">
-          <a-select
-            default-value="1"
-            v-decorator="['dateType', { rules: [{ required: true, message: '请选择日期类型' }] }]"
-          >
-            <a-select-option value="1">年</a-select-option>
-            <a-select-option value="2">年-月</a-select-option>
-            <a-select-option value="3">年-月-日</a-select-option>
-            <a-select-option value="4">年-月-日时：分</a-select-option>
+        <a-form-item
+          label="日期类型"
+          v-if="form.getFieldsValue().type =='日期'"
+          :labelCol="labelCol"
+          :wrapperCol="wrapperCol"
+        >
+          <a-select v-decorator="['dateType', { rules: [{ required: true, message: '请选择日期类型' }] }]">
+            <a-select-option value="年">年</a-select-option>
+            <a-select-option value="年-月">年-月</a-select-option>
+            <a-select-option value="年-月-日">年-月-日</a-select-option>
+            <a-select-option value="年-月-日时：分">年-月-日时：分</a-select-option>
           </a-select>
         </a-form-item>
-        <a-form-item label="单选下拉">
-          <a-select
-            default-value="1"
-            v-decorator="['select', { rules: [{ required: true, message: '请选择日期类型' }] }]"
-          >
-            <a-select-option value="1">选项一</a-select-option>
-            <a-select-option value="2">选项二</a-select-option>
-          </a-select>
-        </a-form-item>
-        <!--<a-form-item>
+        <template v-if="form.getFieldsValue().type =='单选下拉'">
+          <a-form-item label="单选下拉" :labelCol="labelCol" :wrapperCol="wrapperCol">
+            <a-select
+              mode="multiple"
+              allowClear
+              v-decorator="['select', { rules: [{ required: true, message: '请选择单选下拉' }] }]"
+            >
+              <a-select-option value="选项一">选项一</a-select-option>
+              <a-select-option value="选项二">选项二</a-select-option>
+            </a-select>
+          </a-form-item>
+        </template>
+        <a-form-item :labelCol="labelCol" :wrapperCol="wrapperCol">
           <a-checkbox :checked="isCheck" @change="handleChange">是否必填</a-checkbox>
-        </a-form-item>-->
+        </a-form-item>
       </a-form>
     </a-modal>
   </div>
 </template>
-    </a-modal>
-  </div>
+</a-modal>
+</div>
 </template>
 <script>
 const columns = [
@@ -119,7 +115,6 @@ const columns = [
     scopedSlots: { customRender: "operation" }
   }
 ];
-
 const data = [
   {
     key: 0,
@@ -143,103 +138,67 @@ const data = [
     options: "选项一、选项二"
   }
 ];
-// for (let i = 0; i < 100; i++) {
-//   data.push({
-//     key: i.toString(),
-//     name: `字段 ${i}`,
-//     age: 32,
-//     address: `London Park no. ${i}`
-//   });
-// }
 export default {
   data() {
-    this.cacheData = data.map(item => ({ ...item }));
     return {
+      labelCol: { lg: { span: 7 }, sm: { span: 7 } },
+      wrapperCol: { lg: { span: 13 }, sm: { span: 17 } },
       isCheck: false,
+      isShow: false,
       isVisible: false,
       confirmLoading: false,
       form: this.$form.createForm(this),
-
       data,
       columns,
-      editingKey: ""
+      isShowType: "",
+      editIndex: 0
     };
   },
   methods: {
     // 获取item数据
-    getItem(data) {
+    getItem(data, index) {
       console.log("item", data);
       this.isVisible = true;
+      this.editIndex = index;
+      this.isCheck = data.isValid == "是" ? true : false;
       this.$nextTick(() => {
-        this.form.setFieldsValue({
-          name: "设置值"
-        });
+        this.form.setFieldsValue({ ...data });
+        console.log("form", this.form.getFieldsValue());
       });
-      //  this.tableHeaderCoverForm.setFieldsValue({name: '2333'},'keys')
-      console.log("form", this.form.getFieldsValue());
     },
     handleChange(e) {
       this.isCheck = e.target.checked;
       this.$nextTick(() => {
-        this.form.validateFields(["nickname"], { force: true });
+        this.form.validateFields(["isValid"], { force: true });
       });
     },
     handleOk() {
       this.form.validateFields((err, values) => {
         if (!err) {
-          this.isVisible = true;
-          console.log("Received values of form: ", values);
+          this.isVisible = false;
+          console.log("Received values of form: ", this.isCheck, values);
+          if (values.text) {
+            values.options = values.text;
+            delete values.text;
+          }
+          if (values.dateType) {
+            values.options = values.dateType;
+            delete values.dateType;
+          }
+          if (values.select) {
+            values.options = values.select.join(",");
+            delete values.select;
+          }
+          values.isValid = this.isCheck == true ? "是" : "否";
+          this.data[this.editIndex] = values;
+          console.log("update-data", this.data[0]);
         }
       });
     },
     handleCancel() {
+      this.form.resetFields();
       this.isVisible = false;
-    },
-
-    handleChange(value, key, column) {
-      const newData = [...this.data];
-      const target = newData.find(item => key === item.key);
-      if (target) {
-        target[column] = value;
-        this.data = newData;
-      }
-    },
-    handleOk() {}
-    // edit(key) {
-    //   const newData = [...this.data];
-    //   const target = newData.find(item => key === item.key);
-    //   this.editingKey = key;
-    //   if (target) {
-    //     target.editable = true;
-    //     this.data = newData;
-    //   }
-    // },
-    // save(key) {
-    //   const newData = [...this.data];
-    //   const newCacheData = [...this.cacheData];
-    //   const target = newData.find(item => key === item.key);
-    //   const targetCache = newCacheData.find(item => key === item.key);
-    //   if (target && targetCache) {
-    //     delete target.editable;
-    //     this.data = newData;
-    //     Object.assign(targetCache, target);
-    //     this.cacheData = newCacheData;
-    //   }
-    //   this.editingKey = "";
-    // },
-    // cancel(key) {
-    //   const newData = [...this.data];
-    //   const target = newData.find(item => key === item.key);
-    //   this.editingKey = "";
-    //   if (target) {
-    //     Object.assign(
-    //       target,
-    //       this.cacheData.find(item => key === item.key)
-    //     );
-    //     delete target.editable;
-    //     this.data = newData;
-    //   }
-    // }
+    }
   }
 };
 </script>
